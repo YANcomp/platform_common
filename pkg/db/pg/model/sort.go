93947189a5c9fc9c63c0d5f726b7d235e2fb35fa
dnaugolnyi <dnaugolnyi@yandex.ru>
2024-01-31 00:09:37 +0300

    update 31.01.0009

---
+++ pkg/db/pg/model/sort.go
@@ -0,0 +1,31 @@
+package model
+
+import (
+	"github.com/Masterminds/squirrel"
+	"strings"
+)
+
+type (
+	Sort struct {
+		column string
+		order  string
+	}
+)
+
+// NewSort Создание новой сортировки
+func NewSort(column, order string) Sort {
+	order = strings.ToUpper(order)
+	if order != "ASC" && order != "DESC" {
+		order = "ASC"
+	}
+
+	return Sort{
+		column: column,
+		order:  order,
+	}
+}
+
+// UseSelectBuilder Добавление в squirrel.SelectBuilder сортировки
+func (opt Sort) UseSelectBuilder(builder squirrel.SelectBuilder) squirrel.SelectBuilder {
+	return builder.OrderBy(opt.column + " " + opt.order)
+}
